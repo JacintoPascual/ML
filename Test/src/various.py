1f20582a64d63e44104129940129db976e82d34f
Jacinto Pascual Martin <JAPA@statoil.com>
2016-11-25 13:59:07 +0100

    Minor changes

--- Test/src/various.py
+++ Test/src/various.py
@@ -1,9 +1,8 @@
-
+import logging
 from string import Template
 t = Template('${village}folk send $$10 to $cause.')
 s = t.substitute(village='Nottingham', cause='the ditch fund')
-print (s);
-#'Nottinghamfolk send $10 to the ditch fund.'
+print(s)  # 'Nottinghamfolk send $10 to the ditch fund.'
 
 # The substitute() method raises a KeyError when a placeholder is not supplied
 # in a dictionary or a keyword argument
@@ -12,11 +11,10 @@ print (s);
 t = Template('Return the $item to $owner.')
 d = dict(item='unladen swallow')
 s = t.safe_substitute(d)
-print (s);
-#'Return the unladen swallow to $owner.'
+print(s)  # 'Return the unladen swallow to $owner.'
 
-import logging
-# Debug and ingo are by default suppressed
+
+# Debug and info are by default suppressed
 logging.debug('Debugging information')
 logging.info('Informational message')
 logging.warning('Warning:config file %s not found', 'server.conf')
